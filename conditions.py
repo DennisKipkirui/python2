4ae16e2999d6d81a312a2586c75978ff2fd0c1b0
DennisKipkirui <kipkiruimutai33@gmail.com>
2024-09-12 07:03:12 +0300

    If,Else,Else If conditions

---
+++ conditions.py
@@ -0,0 +1,13 @@
+isLate = True
+if isLate:
+    print("I need to call my BOSS")
+
+#If Else with statement
+car_model = "bmw"
+car_year = 1000
+
+if (2023 - car_year) < 10:
+    print(f"Your {car_model} is {2023 - car_year} years old and its almost new we can buy it")
+else:
+    print(f"Sorry Our company doesn't buy {car_model} that age is more than 10")
+    print(f"Your car age is {2023 - car_year}")
